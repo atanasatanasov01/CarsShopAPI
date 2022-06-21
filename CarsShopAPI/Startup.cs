0a640c34a9638df7a6153f4a505bfe30a4628091
atanasatanasov01 <atanasovvv01@gmail.com>
2022-06-21 22:10:03 +0300

    Implemented healthchecks for the API and DB

--- CarsShopAPI/Startup.cs
+++ CarsShopAPI/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Net.Mime;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,12 +13,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Http;
 using RestAPI.Repositories;
 using MongoDB.Driver;
 using RestAPI.Settings;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 
 
@@ -37,10 +41,9 @@ namespace CarsShopAPI
         {
 
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-            
+            var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
             services.AddSingleton<IMongoClient>(serviceProvider => {
-                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-                return new MongoClient(settings.connectionString);
+                return new MongoClient(mongoDbSettings.connectionString);
             });
             
             services.AddSingleton<IItemsRepository, MongoDBItemsRepository>();
@@ -52,6 +55,12 @@ namespace CarsShopAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CarsShopAPI", Version = "v1" });
             });
+
+            services.AddHealthChecks()
+                .AddMongoDb(mongoDbSettings.connectionString, 
+                name: "mongoDbHealthcheck",
+                timeout: TimeSpan.FromSeconds(3),
+                tags: new[] { "ready" });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -73,6 +82,34 @@ namespace CarsShopAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+
+                endpoints.MapHealthChecks("/healthcheck/ready", new HealthCheckOptions{
+                     Predicate = (check) => check.Tags.Contains("ready"),
+                     ResponseWriter = async(context, report) => 
+                     {
+                        var result = JsonSerializer.Serialize(
+                            new {
+                                status = report.Status.ToString(),
+                                checks = report.Entries.Select(entry => new {
+                                    name = entry.Key,
+                                    status = entry.Value.Status.ToString(),
+                                    exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                                    duration = entry.Value.Duration.ToString()
+                                })
+                            }
+                        );
+
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        await context.Response.WriteAsync(result);
+
+                     }
+                });
+
+                endpoints.MapHealthChecks("/healthcheck/live", new HealthCheckOptions{
+                    Predicate = (_) => false
+                });
+
+
             });
         }
     }
